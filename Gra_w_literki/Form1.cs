d3ccd61a5f16137bd30045dff87d6c4e0d2546d7
mirusser <mirusser@gmail.com>
2016-03-21 15:22:17 +0100

    Gra_w_literki
    Cały projekt
    mini Mistrz Klawiatury

---
+++ Gra_w_literki/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gra_w_literki
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Dodajemy losową literkę do kontrolki ListBox
+            listBox1.Items.Add((Keys)random.Next(65,90));
+            if(listBox1.Items.Count >7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Koniec gry");
+                timer1.Stop();
+                if (MessageBox.Show("Chcesz zagrać jeszcze raz?","Koniec gry", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    stats.StatsClear();
+                    correctLabel.Text = "Prawidłowych: " + stats.Correct;
+                    missedLabel.Text = "Błędów: " + stats.Missed;
+                    totalLabel.Text = "Wszystkich: " + stats.Total;
+                    accuracyLabel.Text = "Dokładność: " + stats.Accuracy + "%";
+                    timer1.Interval = 800;
+                    difficultyProgressBar.Value = 0;
+                    timer1.Start();
+                    listBox1.Items.Clear();
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //jeśli gracz nacisnął klawisz literki dostępne w kontrolce
+            //ListBox, to usuwamy ją i zwiększamy tempo gry
+            if(listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+
+                //Gracz nasicnął prawidłowy klawisz, aktualizujemy zatem statysktyki
+                stats.Update(true);
+            }
+            else
+            {
+                //gracz nacisnął nieprawidłowy klawisz, aktualizacja statystyk
+                stats.Update(false);
+            }
+
+            //aktualizujemy etykiety na pasku stanu
+            correctLabel.Text = "Prawidłowych: " + stats.Correct;
+            missedLabel.Text = "Błędów: " + stats.Missed;
+            totalLabel.Text = "Wszystkich: " + stats.Total;
+            accuracyLabel.Text = "Dokładność: " + stats.Accuracy + "%";
+        }
+    }
+}
---
+++ Gra_w_literki/Stats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_w_literki
+{
+    class Stats
+    {
+        public int Total = 0;
+        public int Missed = 0;
+        public int Correct = 0;
+        public int Accuracy = 0;
+
+        public void StatsClear()
+        {
+            Total = 0;
+            Missed = 0;
+            Correct = 0;
+            Accuracy = 0;
+        }
+        
+        public void Update(bool correctKey)
+        {
+            Total++;
+
+            if(!correctKey)
+            {
+                Missed++;
+            }
+            else
+            {
+                Correct++;
+            }
+
+            Accuracy = 100 * Correct / Total;
+        }
+    }
+}
